195b5ba786a82df6ecc76357c4bc7f446c071d44
Kalin Angelov <kalin.st.angelov@gmail.com>
2025-07-19 19:35:44 +0300

    edit: nothing important

--- src/main/java/app/web/WishController.java
+++ src/main/java/app/web/WishController.java
@@ -13,7 +13,7 @@ import java.util.List;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("api/v1/wishes")
+@RequestMapping("/api/v1/wishes")
 public class WishController {
 
     @Autowired
